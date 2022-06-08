98121a93485333645aa511ffa299c2d677d1dd1e
Fidel Besada Juarez <pontianus@gmail.com>
2022-06-08 12:14:03 +0200

    First commit
    Initial project creation

---
+++ src/main/java/com/smart/simplechat/SimpleChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.smart.simplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatServerApplication.class, args);
+	}
+
+}
